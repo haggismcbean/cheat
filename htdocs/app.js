ad2cd530cc9513ce94526ebc8e46db6ac0c7d276
hbishop11 <hbishop11@gmail.com>
2015-04-01 21:56:45 +0100

    added hover functionality

--- htdocs/app.js
+++ htdocs/app.js
@@ -1,3 +1,11 @@
+/***************************************************************************************\
+|	Sources: 																			|
+|	http://www.13thparallel.org/archive/bezier-curves/									|
+|	http://www.purplemath.com/modules/distform.htm 										|
+|	http://www.bbc.co.uk/schools/gcsebitesize/maths/geometry/trigonometryrev2.shtml		|
+|																						|
+\***************************************************************************************/
+
 function Coords(x, y){
 	if(!x){
 		this.x = 0;
@@ -10,10 +18,31 @@ function Coords(x, y){
 }
 
 function QuadraticBezier(c1, c2, c3){
+	var self = this;
+	this.length = 0;
 	this.multiplier = 1.1;
+	this.points = {};
 	this.c1 = c1;
 	this.c2 = c2;
 	this.c3 = c3;
+	this.numberPoints;
+	function init(){
+		self.numberPoints = this.c3.x - this.c1.x;
+		var percent;
+		var point;
+
+		for(var i=0; i < self.numberPoints + 1; i++){
+			percent = i / self.numberPoints;
+			point = self.getPoint(percent);
+			self.points['point' + i] = {};
+			self.points['point' + i].x = point.x;
+			self.points['point' + i].y = point.y;
+			self.points['point' + i].angle = self.getAngle(percent);
+			if(i > 0){
+				self.length += self.getLength(self.points['point' + (i-1)], self.points['point' + i]);
+			}
+		}
+	}
 	this.b1 = function(t){
 		return t*t;
 	};
@@ -47,67 +76,97 @@ function QuadraticBezier(c1, c2, c3){
 		var length = Math.sqrt(Math.pow(c2.x - c1.x, 2) + Math.pow(c2.y - c1.y, 2));
 		return length;
 	}
+	init();
 }
 
-function plotPoints(){
-	for(var point in points) {
-		$("#board").append("<div class='point' id="+point+"></div>");
-		$("#"+point).css({
-			"left": points[point].x + "px",
-			"top": points[point].y + "px",
-			"transform": "rotate(" + points[point].angle +"deg)",
-			"background-color": points[point].colour
-		});
-	}
-}
-
-var c1 = new Coords(50, 500);
-var c2 = new Coords(450, 0);
-var c3 = new Coords(850, 500);
-
-var curve = new QuadraticBezier(c1, c2, c3);
-
-var hand = {};
-
-var numberPoints = 800;
-var numberCards = 10;
-var length = 0;
-
-for(var i=0; i < numberPoints + 1; i++){
-	var percent = i / numberPoints;
-	var position = curve.getPoint(percent);
-	hand['position' + i] = {};
-	hand['position' + i].x = position.x;
-	hand['position' + i].y = position.y;
-	hand['position' + i].angle = curve.getAngle(percent);
-	if(i > 0){
-		length += curve.getLength(hand['position' + (i-1)], hand['position' + i]);
-		hand['position' + i].cumLength = length;
-	}
-}
-var segmentLength = length / (numberCards + 1);
-var points = {};
-var num = 0;
-var colourNum = 0;
-length = 0;
-colours = ["red", "blue", "green", "purple", "yellow"];
-for(var i=0; i < numberPoints + 1; i++){
-	if(i > 0){
-		length += curve.getLength(hand['position' + (i-1)], hand['position' + i]);
-		if(length >= segmentLength){
-			points['position' + num] = {};
-			points['position' + num] = hand['position' + i];
-			points['position' + num].colour = colours[colourNum];
-			num++;
-			colourNum++;
-			if(colourNum > colours.length - 1){
-				colourNum = 0;
+function positionCards(){
+	var card = 0;
+	var requiredSegmentLength = curve.length / (numberCards + 1);
+	var segmentLength = 0;
+	var colourNum = 0;
+	var colours = ["red", "blue", "green", "purple", "yellow", "pink", "orange"];
+	
+	for(var i=0; i < curve.numberPoints + 1; i++){
+		if(i > 0){
+			segmentLength += curve.getLength(curve.points['point' + (i-1)], curve.points['point' + i]);
+			if(segmentLength >= requiredSegmentLength){
+				hand['card' + card] = {};
+				hand['card' + card] = curve.points['point' + i];
+				hand['card' + card].colour = colours[colourNum];
+				card++;
+				colourNum++;
+				if(colourNum > colours.length - 1){
+					colourNum = 0;
+				}
+				segmentLength = 0;
 			}
-			length = 0;
 		}
 	}
 }
 
+function renderCards(){
+	for(var card in hand) {
+		var number = card.slice(4);
+		$("#board").append("<div class='card' id="+card+"></div>");
+		$("#"+card).css({
+			"left": hand[card].x + "px",
+			"top": hand[card].y + "px",
+			"transform": "rotate(" + hand[card].angle +"deg)",
+			"background-color": hand[card].colour,
+			'z-index': number
+		});
+	}
+	$('.card').each(function(){
+		$(this).hover(hoverIn, hoverOut);
+	});
+}
+
+//setting up the view
+var c1 = new Coords(50, 500);
+var c2 = new Coords(450, 200);
+var c3 = new Coords(850, 500);
+
+var curve = new QuadraticBezier(c1, c2, c3);
+
+//given by api. api gives an array of cards I guess, then we get the length.
+var numberCards = 32; 
+var hand = {};
+
+positionCards();
+renderCards();
+
 console.log(JSON.stringify(hand, 0, 2));
 
-plotPoints();
+function hoverIn(){
+	var card = $(this).attr('id');
+	var number = card.slice(4);
+
+	//distance it moves out
+	var movement = 21;
+
+	//angle of card
+	var angle = hand[card].angle * -1;
+
+	//convert angle to radians
+	var radians = angle * (Math.PI / 180);
+
+	var x = movement * Math.sin(radians);
+	var y = movement * Math.cos(radians);
+
+	$(this).css({
+		"left": hand[card].x - x + "px",
+		"top": hand[card].y - y + "px",
+		'z-index': '1000'
+	});
+}
+
+function hoverOut(){
+	var card = $(this).attr('id');
+	var number = card.slice(4);
+	$(this).css({
+		"left": hand[card].x + "px",
+		"top": hand[card].y + "px",
+		"background-color": hand[card].colour,
+		'z-index': number
+	});
+}
